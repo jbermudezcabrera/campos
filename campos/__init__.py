e7785dff14eb9d0d45ff5d6c396ce427ca7d9f78
Juan Manuel Bermúdez Cabrera <jbermudezcabrera@gmail.com>
2016-10-21 10:37:53 -0400

    Initial commit

---
+++ campos/__init__.py
@@ -0,0 +1,1 @@
+__author__ = 'Juan Manuel Bermúdez Cabrera'
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+__author__ = 'Juan Manuel Bermúdez Cabrera'
+
+setup(
+    name='campos',
+    version='1.0.0',
+    description='',
+    url='campos.com',
+    license='',
+
+    author='Juan Manuel Bermúdez Cabrera',
+    author_email='jbermudezcabrera@gmail.com',
+
+    packages=find_packages(),
+    include_package_data=True
+)
